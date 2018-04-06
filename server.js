38e9bcb0fb7b041a1536f17dea240d8640b70496
Unknown <thomas_641991@hotmail.com>
2018-04-06 23:34:32 +0200

    Hotfix for deploy

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '4200';
-app.set('port', port);
+const port = process.env.PORT || 4200;
+app.listen('port', port);
 
 /**
  * Create HTTP server.
